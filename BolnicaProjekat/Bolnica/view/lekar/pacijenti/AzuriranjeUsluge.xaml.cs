baba3ae62acce3328418bf6123c875f7ef35c8ae
laslo-uri-ftn <laslouri@uns.ac.rs>
2021-04-21 05:29:50 +0200

    Lekar - Azuriranje

--- BolnicaProjekat/Bolnica/view/lekar/pacijenti/AzuriranjeUsluge.xaml.cs
+++ BolnicaProjekat/Bolnica/view/lekar/pacijenti/AzuriranjeUsluge.xaml.cs
@@ -25,16 +25,16 @@ namespace Bolnica.view.lekar.pacijenti
         // BACK - PAGES
         private view.lekar.pacijenti.RadniKalendar refRadniKalendar;
 
-        public DateTime NoviPocetak;
+        public DateTime date;
         public Lekar Lekar;
-        public ZdravstvenaUsluga OdabranaUsluga;
+        public ZdravstvenaUsluga OdabranaUsluga { get; set; }
+        public ZdravstvenaUsluga AzuriranaUsluga { get; set; }
 
         public AzuriranjeUsluge(Lekar Lekar, ZdravstvenaUsluga OdabranaUsluga)
         {
             InitializeComponent();
             this.OdabranaUsluga = OdabranaUsluga;
             this.Lekar = Lekar;
-
             TipUsluge.Text = (OdabranaUsluga.TipUsluge).ToString();
             Prostorija p = Repozitorijum.ProstorijeRepozitorijum.GetInstance.GetProstorijaById(OdabranaUsluga.IdProstorije);
             Prostorija.Text = p.BrojSprat;
@@ -42,6 +42,47 @@ namespace Bolnica.view.lekar.pacijenti
             VremeZavrsetkaUsluge.Text = (OdabranaUsluga.Termin.Kraj).ToString("dddd, dd MMMM yyyy") + " - " + (OdabranaUsluga.Termin.Kraj).ToString("hh:mm tt");
             RazlogZakazivanja.Text = (OdabranaUsluga.RazlogZakazivanja).ToString();
         }
+        private void datum_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            date = datum.SelectedDate.Value;
 
+            if (DateTime.Compare(date, DateTime.Now) < 0)
+            {
+                MessageBox.Show("Moguce je izabrati samo buduce datume");
+                date = DateTime.Now;
+            }
+        }
+
+        private void PotvrdiAzuriranjeButton(object sender, RoutedEventArgs e)
+        {
+            int pocetakSati = Convert.ToInt32(VremePocetakTermina_Sat.Text);
+            int pocetakMinute = Convert.ToInt32(VremePocetakTermina_Minut.Text);
+            string pocetakAP = VremePocetakTermina_AM_PM.Text;
+
+            if (pocetakAP.Equals("PM"))
+                pocetakSati += 12;
+
+
+            date = datum.SelectedDate.Value;
+
+            DateTime pocetak = new DateTime(date.Year, date.Month, date.Day, pocetakSati, pocetakMinute, 0);
+            TimeSpan trajanje = new TimeSpan(0, 0, 30, 0);
+            DateTime kraj = pocetak + trajanje;
+
+            int idUsluge = OdabranaUsluga.Id;
+            int idLekara = OdabranaUsluga.IdLekara;
+            int idPacijenta = OdabranaUsluga.IdPacijenta;
+            TipUsluge tipUsluge = OdabranaUsluga.TipUsluge;
+            int idProstorije = OdabranaUsluga.IdProstorije;
+            bool obavljena = OdabranaUsluga.Obavljena;
+            string razlogZakazivanja = OdabranaUsluga.RazlogZakazivanja;
+            string rezultat = OdabranaUsluga.RezultatUsluge;
+
+            Termin newTermin = new Termin(pocetak, kraj);
+            AzuriranaUsluga = new ZdravstvenaUsluga(newTermin, idUsluge, idLekara, idPacijenta, tipUsluge, idProstorije, obavljena, razlogZakazivanja, rezultat);
+            Repozitorijum.ZdravstvenaUslugaRepozitorijum.GetInstance.DodajUslugu(AzuriranaUsluga);
+            Repozitorijum.ZdravstvenaUslugaRepozitorijum.GetInstance.ObrisiUslugu(OdabranaUsluga);
+
+        }
     }
 }

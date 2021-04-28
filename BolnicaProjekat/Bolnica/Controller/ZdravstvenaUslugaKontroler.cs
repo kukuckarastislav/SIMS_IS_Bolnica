9bfd16ea40a6b325020ac96825eb46bf76a7f3ae
mihajlo-ostojic <ostojic.ra134.2018@uns.ac.rs>
2021-04-28 10:01:30 +0200

    refaktorisanje servisa zdravstvene usluge

--- BolnicaProjekat/Bolnica/Controller/ZdravstvenaUslugaKontroler.cs
+++ BolnicaProjekat/Bolnica/Controller/ZdravstvenaUslugaKontroler.cs
@@ -55,7 +55,7 @@ namespace Controller
         {
             DateTime pocetak = new DateTime(datum.Year, datum.Month, datum.Day, 0, 0, 00);
             DateTime kraj = new DateTime(datum.Year, datum.Month, datum.Day, 23, 59, 00);
-            return ZdravstvenaUslugaServis.getAppointments(OdabraniLekar,pocetak,kraj);
+            return ZdravstvenaUslugaServis.GetSlobodniTerminiLekara(OdabraniLekar,pocetak,kraj);
         }
     }
 }

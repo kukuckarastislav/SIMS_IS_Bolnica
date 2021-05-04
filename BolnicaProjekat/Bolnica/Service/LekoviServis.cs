a9968212ad33071063bc69e16360c9a8178bbf24
Milica Vucinic <vucinic.ra139.2018@uns.ac.rs>
2021-05-04 14:17:57 +0200

    uradjeno sve osim azuriranja

---
+++ BolnicaProjekat/Bolnica/Service/LekoviServis.cs
@@ -0,0 +1,56 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servis
+{
+    class LekoviServis
+    {
+        public void PosaljiLekoveNaReviziju(ObservableCollection<Lekar> odabraniLekari, Lek lek)
+        {
+            throw new NotImplementedException();
+
+            //ovdje prolazi kroz sve lekare itd..
+        }
+
+        public ObservableCollection<Lek> GetOdobreniLekovi()
+        {
+            ObservableCollection<Lek> ret = new ObservableCollection<Lek>();
+            ObservableCollection<Lek> lekovi = Repozitorijum.LekoviRepozitorijum.GetInstance.GetAll();
+            foreach (Lek l in lekovi)
+            {
+                if (l.Odobren == true)
+                    ret.Add(l);
+            }
+            return ret;
+        }
+
+        public ObservableCollection<Lek> GetNeodobreniLekovi()
+        {
+            ObservableCollection<Lek> ret = new ObservableCollection<Lek>();
+            ObservableCollection<Lek> lekovi = Repozitorijum.LekoviRepozitorijum.GetInstance.GetAll();
+            foreach (Lek l in lekovi)
+            {
+                if (l.Odobren == false)
+                    ret.Add(l);
+            }
+            return ret;
+        }
+
+        public Lek ObrisiLek(Lek lek)
+        {
+            return Repozitorijum.LekoviRepozitorijum.GetInstance.ObrisiLek(lek);
+        }
+
+        
+        public Lek DodajLek(Lek lek)
+        {
+            return Repozitorijum.LekoviRepozitorijum.GetInstance.DodajLek(lek);
+        }
+        
+    }
+}

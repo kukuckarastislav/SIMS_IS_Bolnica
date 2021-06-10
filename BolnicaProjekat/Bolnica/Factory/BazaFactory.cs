11229f80812ee6c259df5e79f5919bf4ef16ce55
kukuckarastislav <kukuckarastislav@uns.ac.rs>
2021-06-10 03:51:28 +0200

    Prostorije repozitor Dependency Injectio i Factory

---
+++ BolnicaProjekat/Bolnica/Factory/BazaFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interface;
+
+namespace Factory
+{
+    public class BazaFactory
+    {
+        public static IBaza GetBaza(string nazivBaze)
+        {
+            IBaza baza = null;
+
+            if(nazivBaze == "JSON")
+            {
+                baza = new JSONBaza();
+            }else if(nazivBaze == "SQL")
+            {
+                // Sql baza
+            }
+            else
+            {
+                baza = new JSONBaza();
+            }
+
+            return baza;
+        }
+    }
+}
